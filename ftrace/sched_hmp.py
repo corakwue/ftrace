a3ba31558254d1818e27be1f42361535133537bf
Chukwuchebem Orakwue <nuckies0075@gmail.com>
2016-03-23 13:21:58 -0700

    added descriptions for hmp core features

--- ftrace/sched_hmp.py
+++ ftrace/sched_hmp.py
@@ -23,10 +23,44 @@ from .common import ConstantBase
 from collections import defaultdict
 
 class HMPMigrate(ConstantBase):
+
+##    When a task that was previously idle becomes ready to run,
+##    the scheduler needs to decide which processor will execute the task.
+##    To choose between big and LITTLE cores, the ARM MP solution uses the
+##    tracked load history of a task.
+
     WAKEUP = ()
+
+##    Fork migration operates when the fork system call is used to ceate a new
+##    software thread. At this point, clearly no historical load
+##    information is available ? the thread is new.
+##    The system defaults to a big core for new threads on the assumption that
+##    a ?light? thread will quickly migrate down to a LITTLE core as a result
+##    of wake migration
     FORCE = ()
+
+##    The big.LITTLE MP solution requires that normal scheduler
+##    load balancing be disabled. The downside of this is that long-running
+##    threads can concentrate on the big cores, leaving the LITTLE cores
+##    idle and under-utilized. Overal system performance, in this situation,
+##    can clearly be improved by utilizing all the cores.
+##    Offload migration works to peridically migrate threads downwards
+##    to LITTLE cores to make use of unused compute capacity.
+##    Threads which are migrated downwards in this way remain candidates for
+##    up migration if they exceed the threshold at next scheduling opportunity
+
     OFFLOAD = ()
+
+##    Idle Pull Migration is designed to make best use of active big cores.
+##    When a big core has no task to run, a check is made on
+##    all LITTLE cores to see if a currently running task on a
+##    LITTLE core has a higher load metric that the up migration threshold.
+##    Such a task can then be immediately migrated to the idle big core.
+##    If no suitable task is found, then the big core can be powered down.
+
     IDLE_PULL = ()
+
+## What is this?
     UNKNOWN = ()
 
 # linux/sched.h
